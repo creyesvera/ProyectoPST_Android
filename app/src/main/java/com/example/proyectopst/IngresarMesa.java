0723dcadecb7e988b66e361fef54058b1cd7ea5c
Your Name <camdorey@espol.edu.ec>
2023-01-05 18:22:52 -0500

    Initial commit

---
+++ app/src/main/java/com/example/proyectopst/IngresarMesa.java
@@ -0,0 +1,14 @@
+package com.example.proyectopst;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IngresarMesa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingresar_mesa);
+    }
+}
