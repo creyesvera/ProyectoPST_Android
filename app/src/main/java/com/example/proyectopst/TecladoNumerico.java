f53b542b55b2c9782944727db1da9d5cf8bd2e36
stevenijm <yosoystevenijm@gmail.com>
2023-01-10 00:17:17 -0500

    antes de corregir
      button0 = (Button) findViewById(R.id.button_1);

--- app/src/main/java/com/example/proyectopst/TecladoNumerico.java
+++ app/src/main/java/com/example/proyectopst/TecladoNumerico.java
@@ -10,37 +10,128 @@ import android.widget.EditText;
 
 
 public class TecladoNumerico extends AppCompatActivity {
-    EditText Number = (EditText) findViewById(R.id.number);
-    Button buttonClear = (Button) findViewById(R.id.button_clear);
+    EditText number;
+    Button buttonClear;
+    Button button0;
+    Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
+    Button button5;
+    Button button6;
+    Button button7;
+    Button button8;
+    Button button9;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        number = (EditText) findViewById(R.id.number);
+        buttonClear = (Button) findViewById(R.id.button_clear);
+        button0 = (Button) findViewById(R.id.button_1);
+        button1 = (Button) findViewById(R.id.button_1);
+        button2 = (Button) findViewById(R.id.button_2);
+        button3 = (Button) findViewById(R.id.button_3);
+        button4 = (Button) findViewById(R.id.button_4);
+        button5 = (Button) findViewById(R.id.button_5);
+        button6 = (Button) findViewById(R.id.button_6);
+        button7 = (Button) findViewById(R.id.button_7);
+        button8 = (Button) findViewById(R.id.button_8);
+        button9 = (Button) findViewById(R.id.button_9);
+
         buttonClear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                onClearClick(v);
+                onClearClick();
+            }
+        });
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberClick(v);
+            }
+        });
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberClick(v);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberClick(v);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberClick(v);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberClick(v);
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberClick(v);
+            }
+        });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberClick(v);
+            }
+        });
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberClick(v);
+            }
+        });
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberClick(v);
+            }
+        });
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberClick(v);
+            }
+        });
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberClick(v);
             }
         });
     }
 
-    public void onNumberClick(View view) {
-        Button button = (Button) view;
-        String input = button.getText().toString();
-        String currentText = Number.getText().toString();
-        Number.setText(currentText + input);
+    private void onNumberClick(View v) {
+        Button button = (Button) v;
+        String text = button.getText().toString();
+        number.setText(text);
     }
 
-    public void onClearClick(View view) {
-        Number.setText("");
+    public void onClearClick() {
+        number.setText("");
     }
 
 
 
     public void onClearClickBinding(View view) {
-            onClearClick(view);
+            onClearClick();
         }
 
 }
