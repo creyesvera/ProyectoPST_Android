81724a8b3f327101b864d2bb3b4be2d383868217
stevenijm <yosoystevenijm@gmail.com>
2023-01-22 16:43:12 -0500

    crear cuenta

--- app/src/main/java/com/example/proyectopst/MainActivity.java
+++ app/src/main/java/com/example/proyectopst/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
     //se conecta a la base de datos
 
-    private String server = "192.168.200.26";
+    private String server = "192.168.100.6";
     private int port = 8080;
     // definir imagenes
     private ImageView imageSelect;
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
     private RequestQueue requestQueue;
     private EditText usernameEditText;
     private EditText passwordEditText;
-
+    private String avatar = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -88,6 +88,10 @@ public class MainActivity extends AppCompatActivity {
                     int tag = (int) view.getTag();
                     // Establecer imagen seleccionada en ImageSelect
                     imageSelect.setImageResource(tag);
+                    // Obtener el nombre de la imagen seleccionada
+                    avatar = randomImageName;
+                    Toast.makeText(getApplicationContext(), "img: "
+                            + avatar, Toast.LENGTH_SHORT).show();
                 }
             });
         }
@@ -95,69 +99,115 @@ public class MainActivity extends AppCompatActivity {
 
     //se incia seción y se verifica la existencia del usuario
     public void inicio_sesion(View view) {
-        //Consultar con la base de datos si el usuario existe
-        StringRequest request = new StringRequest(
-                Request.Method.GET,
-                "http://"+server+":"+port+"/android/SELECT id, puntaje, estrellas from usuarios WHERE nombre= '" + usernameEditText.getText().toString() + "' && contrasena= '" + passwordEditText.getText().toString() + "'",
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
+        // Verificar si los campos de usuario y contraseña están llenos
+        if(usernameEditText.getText().toString().equals("") || passwordEditText.getText().toString().equals("")) {
+            Toast.makeText(getApplicationContext(), "Por favor llene todos los campos", Toast.LENGTH_SHORT).show();
+        } else if(avatar.equals("")) {
+            Toast.makeText(getApplicationContext(), "Por favor seleccione una imagen", Toast.LENGTH_SHORT).show();
+        } else {
+            //Consultar con la base de datos si el usuario existe
+            StringRequest request = new StringRequest(
+                    Request.Method.GET,
+                    "http://" + server + ":" + port + "/android/SELECT id, puntaje, estrellas from usuarios WHERE nombre= '" + usernameEditText.getText().toString() + "' && contrasena= '" + passwordEditText.getText().toString() + "'",
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
 
-                        if(response != null){
-                            if(response.equals("Tabla vacía")){
-                                usernameEditText.setText("usuario o contrasena incorrecta");
-                            }
-                            else {
-                                String[] cadena = new String[0];
-                                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-                                    cadena = Arrays.stream(response.split("\\r\\n")).toArray(String[]::new);
-                                }
-                                String[] datos = new String[0];
-                                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-                                    datos = Arrays.stream(cadena[3].split(" \\t\\t\\t| ")).toArray(String[]::new);
-                                }
-                                int id = Integer.parseInt(datos[0]);
-                                int puntaje = Integer.parseInt(datos[2]);
-                                int estrellas = Integer.parseInt(datos[4]);
+                            if (response != null) {
+                                if (response.equals("Tabla vacía")) {
+                                    usernameEditText.setText("usuario o contrasena incorrecta");
+                                } else {
+                                    String[] cadena = new String[0];
+                                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                                        cadena = Arrays.stream(response.split("\\r\\n")).toArray(String[]::new);
+                                    }
+                                    String[] datos = new String[0];
+                                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                                        datos = Arrays.stream(cadena[3].split(" \\t\\t\\t| ")).toArray(String[]::new);
+                                    }
+                                    int id = Integer.parseInt(datos[0]);
+                                    int puntaje = Integer.parseInt(datos[2]);
+                                    int estrellas = Integer.parseInt(datos[4]);
 
-                                //EditText et1 = (EditText) findViewById(R.id.nickname);
-                                Intent i = new Intent(MainActivity.this, IngresarMesa.class);
-                                i.putExtra("server", server);
-                                i.putExtra("port", port);
-                                i.putExtra("id", id);
-                                i.putExtra("puntaje", puntaje);
-                                i.putExtra("estrellas", estrellas);
-                                startActivity(i);
+                                    //EditText et1 = (EditText) findViewById(R.id.nickname);
+                                    Intent i = new Intent(MainActivity.this, IngresarMesa.class);
+                                    i.putExtra("server", server);
+                                    i.putExtra("port", port);
+                                    i.putExtra("id", id);
+                                    i.putExtra("puntaje", puntaje);
+                                    i.putExtra("estrellas", estrellas);
+                                    i.putExtra("avatar", avatar);
+                                    startActivity(i);
+                                }
+
+                            } else {
+                                usernameEditText.setText("Error");
+
                             }
 
                         }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
 
-                        else {
-                             usernameEditText.setText("Error");
                         }
-
                     }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-
-                    }
-                }
-        );
-        requestQueue.add(request);
-
+            );
+            requestQueue.add(request);
+        }
     }
 
 
     //se crea el usuario y se añade a la base de datos
     public void crear_cuenta(View view) {
-        //EditText et1 = (EditText) findViewById(R.id.nickname);
-        Intent i = new Intent(MainActivity.this, LoginUser.class );
+        // Verificar si los campos de usuario y contraseña están llenos
+        if(usernameEditText.getText().toString().equals("") || passwordEditText.getText().toString().equals("")) {
+            Toast.makeText(getApplicationContext(), "Por favor llene todos los campos", Toast.LENGTH_SHORT).show();
+        } else if(avatar.equals("")) {
+            Toast.makeText(getApplicationContext(), "Por favor seleccione una imagen", Toast.LENGTH_SHORT).show();
+        } else {
+            // Subir los datos del usuario a la tabla de usuarios en la base de datos
+            StringRequest request = new StringRequest(
+                    Request.Method.POST,
+                    "http://" + server + ":" + port + "/android/insert/INSERT INTO usuarios (nombre, contrasena, avatar) VALUES ('" + usernameEditText.getText().toString() + "', '" + passwordEditText.getText().toString() + "', '" + avatar + "')",
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
 
-        i.putExtra("server", server);
-        i.putExtra("port", port);
-        //i.putExtra("direccion", et1.getText().toString());
-        startActivity(i);
+                            if (response != null) {
+                                if (response.equals("Usuario creado")) {
+                                    Toast.makeText(getApplicationContext(), "Cuenta creada exitosamente", Toast.LENGTH_SHORT).show();
+                                    //puede ser que se quiera redirigir a login
+                                    // Pasar al siguiente activity
+                                    Intent intent = new Intent(MainActivity.this, IngresarMesa.class);
+                                    intent.putExtra("server", server);
+                                    intent.putExtra("port", port);
+                                    intent.putExtra("id", usernameEditText.getText().toString());
+                                    intent.putExtra("puntaje", 0);
+                                    intent.putExtra("estrellas", 0);
+                                    intent.putExtra("avatar", avatar);
+                                    startActivity(intent);
+
+                                } else {
+                                    Toast.makeText(getApplicationContext(), "Error al crear cuenta", Toast.LENGTH_SHORT).show();
+                                }
+
+                            } else {
+                                Toast.makeText(getApplicationContext(), "Error al crear cuenta", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+
+                        }
+                    }
+            );
+            requestQueue.add(request);
+        }
     }
+
 }
